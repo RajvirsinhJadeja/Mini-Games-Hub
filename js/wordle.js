c88464145968142e4b16600c72381035dca8b79a
Rajvirsinh <rjs.jadeja@gmail.com>
2025-07-10 17:06:10 -0700

    Added cells to grid and make its size dynamic

--- js/wordle.js
+++ js/wordle.js
@@ -1,4 +1,42 @@
 let word = [];
+const set = new Set();
+let grid = [[]];
+
+gameLoop();
+
+function gameLoop() {
+  getWord();
+  createWordleGrid();
+}
+
+function createWordleGrid() {
+  const gridContainer = document.querySelector(".wordle-grid");
+
+  for (let row = 0; row < 6; row++) {
+    grid[row] = [];
+    for (let col = 0; col < 5; col++) {
+      const cell = document.createElement("div");
+      const textNum = document.createElement("p");
+
+      cell.classList.add("cell");
+      textNum.classList.add("cell-num");
+
+      cell.style.display = "flex";
+      cell.style.alignItems = "center";
+      cell.style.justifyContent = "center";
+      cell.style.width = "clamp(60px, 13vw, 85px)";
+      cell.style.height = "clamp(60px, 13vw, 85px)";
+      cell.style.border = "2px solid white";
+
+      textNum.style.color = "white";
+
+      cell.appendChild(textNum);
+      gridContainer.appendChild(cell);
+
+      grid[row][col] = cell;
+    }
+  }
+}
 
 async function getWord() {
   try {
@@ -9,14 +47,14 @@ async function getWord() {
     console.log(test[0]);
     for (let i = 0; i < 5; i++) {
       word.push(test[0].charAt(i));
+      set.add(test[0].charAt(i));
     }
     console.log(word);
+    console.log(set);
   } catch (error) {
     console.error(error);
   }
 }
-
-getWord();
 
 document.addEventListener("keydown", function (e) {
   if (e.key.length === 1 && /[a-zA-Z]/.test(e.key)) {
