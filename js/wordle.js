6178613ed0a3dcf6f1c7fc3770bca55a25f3b02e
Rajvirsinh <rjs.jadeja@gmail.com>
2025-07-11 17:47:17 -0700

    Keyboard inputs now affect wordle grid

--- js/wordle.js
+++ js/wordle.js
@@ -1,12 +1,40 @@
 let word = [];
 const set = new Set();
 let grid = [[]];
+let rowCount = 0;
+let colCount = 0;
+let gameOver = false;
 
 gameLoop();
 
 function gameLoop() {
   getWord();
   createWordleGrid();
+
+  document.addEventListener("keydown", handleKeyPress);
+
+  const letterButtons = document.querySelectorAll(".letter");
+  letterButtons.forEach((charButton) =>
+    charButton.addEventListener("click", handleClickPress)
+  );
+}
+
+async function getWord() {
+  try {
+    const response = await fetch(
+      "https://random-word-api.herokuapp.com/word?length=5"
+    );
+    const test = await response.json();
+    console.log(test[0]);
+    for (let i = 0; i < 5; i++) {
+      word.push(test[0].charAt(i));
+      set.add(test[0].charAt(i));
+    }
+    console.log(word);
+    console.log(set);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function createWordleGrid() {
@@ -28,8 +56,6 @@ function createWordleGrid() {
       cell.style.height = "clamp(60px, 13vw, 85px)";
       cell.style.border = "2px solid white";
 
-      textNum.style.color = "white";
-
       cell.appendChild(textNum);
       gridContainer.appendChild(cell);
 
@@ -38,30 +64,53 @@ function createWordleGrid() {
   }
 }
 
-async function getWord() {
-  try {
-    const response = await fetch(
-      "https://random-word-api.herokuapp.com/word?length=5"
-    );
-    const test = await response.json();
-    console.log(test[0]);
-    for (let i = 0; i < 5; i++) {
-      word.push(test[0].charAt(i));
-      set.add(test[0].charAt(i));
-    }
-    console.log(word);
-    console.log(set);
-  } catch (error) {
-    console.error(error);
+function handleInput(input) {
+  let textContainer = grid[rowCount][colCount];
+
+  if (textContainer.innerText == "") textContainer.textContent = input;
+
+  textContainer.style.color = "white";
+  textContainer.style.fontSize = "1.5rem";
+
+  if (colCount < 4) colCount++;
+}
+
+function handleEnter() {
+  if (colCount === 4) {
+    rowCount++;
+    colCount = 0;
+  }
+
+  if (rowCount === 5 && colCount === 4) gameOver = true;
+}
+
+function handleBackspace() {
+  let textContainer = grid[rowCount][colCount];
+  if (colCount === 4 && textContainer.innerText != "")
+    textContainer.textContent = "";
+  else if (colCount > 0) colCount--;
+
+  textContainer = grid[rowCount][colCount];
+
+  textContainer.textContent = "";
+}
+
+function handleKeyPress(e) {
+  if (gameOver) return;
+
+  if (e.key.length === 1 && /[a-zA-Z]/.test(e.key)) {
+    handleInput(e.key.toUpperCase());
+  } else if (e.key === "Enter") {
+    handleEnter();
+  } else if (e.key === "Backspace") {
+    handleBackspace();
   }
 }
 
-document.addEventListener("keydown", function (e) {
-  if (e.key.length === 1 && /[a-zA-Z]/.test(e.key)) {
-    console.log("typed a key: " + e.key.toLowerCase());
-  } else if (e.key === "Enter") {
-    console.log("Pressed Enter!");
-  } else if (e.key === "Backspace") {
-    console.log("Pressed Backspace!!");
-  }
-});
+function handleClickPress(e) {
+  if (gameOver) return;
+
+  const clickedElement = e.target;
+
+  handleInput(clickedElement.innerText);
+}
